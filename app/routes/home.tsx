9a0740a40679801cc82ea5bb58ff2a9cc55ed78c
Vanhaeren Antonius <antonius.vanhaeren.av@gmail.com>
2025-08-17 18:25:18 +0200

    init app

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'The Friendly dev' },
+    { name: 'description', content: 'Custom website development' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>My app</>;
 }
