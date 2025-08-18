40665cedd1787278c15c79b369ad58b274579f87
Vanhaeren Antonius <antonius.vanhaeren.av@gmail.com>
2025-08-18 11:34:58 +0200

    added details page projects in clientloadervmode

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,10 +1,36 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+import type { Project } from '~/types';
+import ProjectCard from '~/components/ProjectCard';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'The Friendly Dev | Projects' },
+    { name: 'description', content: 'My website project portfolio' },
+  ];
+}
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+
   return (
-    <>
+    <section>
       <h2 className='text-3xl font-bold text-white mb-8 text-center'>
-        🚀 Projects
+        🚀 Projects{' '}
       </h2>
-    </>
+      <div className='grid gap-6 sm:grid-cols-2'>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </section>
   );
 };
 
